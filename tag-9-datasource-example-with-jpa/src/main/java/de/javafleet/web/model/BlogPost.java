287a78ec01102aef280f9a281dcded3f1668da25
ea234 <anyang@gmx.de>
2025-11-21 11:56:15 +0100

    Fehlerkorrektur: - unterschiedliche Tabellennamen gefixed - unterschiedliche Feldnamen - Abfrage in BlogPostDao.findAll hinzugefügt - Attributnamen zwischen jsp und Servlet (BlogServlet)

--- tag-9-datasource-example-with-jpa/src/main/java/de/javafleet/web/model/BlogPost.java
+++ tag-9-datasource-example-with-jpa/src/main/java/de/javafleet/web/model/BlogPost.java
@@ -1,5 +1,6 @@
 package de.javafleet.web.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,7 +25,10 @@ public class BlogPost implements Serializable {
     private String title;
     private String content;
     private String author;
+    @Column(name = "created_at" )
     private LocalDateTime createdAt;
+    
+    @Column(name = "updated_at" )    
     private LocalDateTime updatedAt;
     
     // Konstruktoren
